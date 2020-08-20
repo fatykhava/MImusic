46dcb7b0619fbc8f579805fc03e54d04c0fc2295
fatykhava <68748929+fatykhava@users.noreply.github.com>
2020-08-20 13:29:39 +0300

    new site v.1

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	$(".slider").slick({
+		arrows: false,
+		dots: true,
+		speed: 500,
+		infinite: true,
+		autoplay: true,
+		autoplaySpeed: 10000,
+		waitForAnimate:true,
+		fade: true,
+})
+});
