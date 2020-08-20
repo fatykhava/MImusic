6f24d34919dd7e3e98db954578b8b77a1832e626
fatykhava <68748929+fatykhava@users.noreply.github.com>
2020-08-20 13:42:57 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,4 +1,36 @@
-$(document).ready(function(){
+'use strict';
+
+function Tabs() {
+	var bindAll = function () {
+		var menuElements = document.querySelectorAll('[data-tab]');
+		for (var i = 0; i < menuElements.length; i++) {
+			menuElements[i].addEventListener('click', change, false);
+		}
+	}
+
+	var clear = function () {
+		var menuElements = document.querySelectorAll('[data-tab]');
+		for (var i = 0; i < menuElements.length; i++) {
+			menuElements[i].classList.remove('active');
+			var id = menuElements[i].getAttribute('data-tab');
+			document.getElementById(id).classList.remove('active');
+		}
+	}
+
+	var change = function (e) {
+		clear();
+		e.target.classList.add('active');
+		var id = e.currentTarget.getAttribute('data-tab');
+		document.getElementById(id).classList.add('active');
+	}
+
+	bindAll();
+}
+
+var connectTabs = new Tabs();
+
+
+$(document).ready(function () {
 	$(".slider").slick({
 		arrows: false,
 		dots: true,
@@ -6,7 +38,15 @@ $(document).ready(function(){
 		infinite: true,
 		autoplay: true,
 		autoplaySpeed: 10000,
-		waitForAnimate:true,
+		waitForAnimate: true,
 		fade: true,
-})
+	})
+});
+
+$(".spoiler__wr-title").click(function (event) {
+	if ($(".wr-blog__spoiler").hasClass("accordion")) {
+		$(".spoiler__wr-title").not($(this)).removeClass("active");
+		$(".spoiler__text").not($(this).next()).slideUp(400);
+	}
+	$(this).toggleClass("active").next().slideToggle(400);
 });
